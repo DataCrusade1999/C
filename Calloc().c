3c9198a76201d6706bb090b18e42ab777be72665
Ashutosh Pandey <ashutosh.pandeyhlr007@gmail.com>
2020-09-04 23:27:06 +0530

    Add files via upload

---
+++ Calloc().c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main() {
+	int n, i, * ptr;
+	printf("Enter The size of Array\t");
+	scanf_s("%d", &n);
+	ptr = (int*)calloc(n ,sizeof(int));
+	if (ptr == NULL) {
+		printf("Memory Allocation Has Been Failed");
+	}
+	else {
+		printf("Memory Allocation Has Been Sucessful\n");
+		printf("Enter The Elements of The Array\t");
+		for (i = 0; i < n; i++) {
+			scanf_s("%d", &ptr[i]);
+		}
+		printf("The Elements Of The Array Are");
+		for (i = 0; i < n; i++) {
+			printf("%d\t", ptr[i]);
+		}
+	}
+}

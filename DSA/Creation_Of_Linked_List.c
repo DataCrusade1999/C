65d4b3895866c575347d65f7592077ad38bb1fa8
Ashutosh Pandey <ashutosh.pandeyhlr007@gmail.com>
2020-09-14 13:46:52 +0530

    Add files via upload

---
+++ DSA/Creation_Of_Linked_List.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	struct node
+	{
+		int data;
+		struct node* link;
+	};
+	struct node* head, * ptr, * current;
+	int n;
+	head = NULL;
+	do
+	{
+		ptr = (struct node*)malloc(sizeof(struct node));
+		if (ptr == NULL) {
+			printf("Memory Allocation Has Been Failed");
+		}
+		if (head == NULL) {
+			head = ptr;
+			current = ptr;
+			current->link = NULL;
+			printf("Enter The Data\n");
+			scanf("%d", &current->data);
+		}
+		else {
+			current->link = ptr;
+			current = ptr;
+			current->link = NULL;
+			printf("Enter The Data\n");
+			scanf("%d", &current->data);
+
+		}
+		printf("Enter 1 If You Want To Create More Nodes\n");
+		scanf("%d", &n);
+	} while (n == 1);
+	printf("Your Nodes Are\n");
+	current = head;
+	while (current != 0) {
+		printf("%d\n", current->data);
+		current = current->link;
+	}
+}

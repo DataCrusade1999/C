e993d69ef6e06f06c09a94d896f8e43b294e18ca
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2021-02-09 15:22:17 +0530

    Added Binary Tree

--- DSA/Tree/Binary_Tree.c
+++ DSA/Tree/Binary_Tree.c
@@ -7,3 +7,31 @@ struct node{
     struct node *right;
 };
 
+
+struct node *create(){
+    int x;
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data in the node;To exit enter -1\n");
+    scanf("%d",&x);
+    if (x==-1)
+    {
+        return 0;
+    }
+
+
+    newnode->data = x;
+    printf("Enter the left child\n");
+    newnode->left = create();
+    printf("Enter the right child\n");
+    newnode->right = create();
+
+    return newnode;
+
+
+}
+
+void main(){
+    struct node *rootptr=0;
+    rootptr = create();
+}
